54ad536a4db22979b3eff667341733fab7b0faf1
alceil <ashishshajisrampickal@gmail.com>
2021-07-04 17:47:44 +0530

    first commit

---
+++ src/components/Count.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Count = ({count}) => {
+    return (
+        <div>
+                <div className="count">
+      <h4>{count}</h4>
+    </div>
+        </div>
+    )
+}
+
+export default Count
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { MdDelete } from "react-icons/md";
+const Footer = () => {
+    return (
+      <div className="footer">
+      <p>Ⓒ Programmig with shahan</p>
+    </div>
+    )
+}
+
+export default Footer
